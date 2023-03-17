729e3b8c49670adfa97ff13cfde020ff2c1fbe09
GAVINARSutherland <asutherland@stcharlessd.org>
2023-03-17 12:39:01 -0500

    Final code cleanup

--- app.js
+++ app.js
@@ -28,10 +28,11 @@ app.listen(3000, () => {
 
 app.get("/FBLA", (req, res) => {
     res.render('index');
-    console.log("rendered");
 });
 
 app.get("/FBLA/dashboard", async (req, res) => {
+    // Renders the dashboard
+    // This block appears quite often. It is used to get the events for the school
     let schoolEvents = [];
     let teacher = await Teacher.findById(signedInUser._id);
     let school = await School.findById(teacher.schoolId);
@@ -46,6 +47,7 @@ app.get("/FBLA/dashboard", async (req, res) => {
 });
 
 app.get("/FBLA/events", async (req, res) => {
+    // Get all the events for the school. Similar to dashboard but is handled differently in the ejs
     let schoolEvents = [];
     let teacher = await Teacher.findById(signedInUser._id);
     let school = await School.findById(teacher.schoolId);
@@ -56,15 +58,14 @@ app.get("/FBLA/events", async (req, res) => {
     res.render('events', {
         schoolEvents
     });
-    console.log("rendered");
 });
 
 app.get("/FBLA/addEvent", (req, res) => {
     res.render('addEvent');
-    console.log("rendered");
 });
 
 app.get("/FBLA/editEvent", async (req, res) => {
+    // Gets all of the events for the school and renders them as a dropdown. The user can then select an event to edit
     let schoolEvents = [];
     let teacher = await Teacher.findById(signedInUser._id);
     let school = await School.findById(teacher.schoolId);
@@ -75,10 +76,10 @@ app.get("/FBLA/editEvent", async (req, res) => {
     res.render('editEvent', {
         schoolEvents
     });
-    console.log("rendered");
 });
 
 app.get("/FBLA/assign-points", async (req, res) => {
+    // Same event dropdown as editEvent, but it also renders the grades and converts them to radio buttons
     let schoolEvents = [];
     let teacher = await Teacher.findById(signedInUser._id);
     let school = await School.findById(teacher.schoolId);
@@ -93,10 +94,10 @@ app.get("/FBLA/assign-points", async (req, res) => {
         schoolEvents,
         grades
     });
-    console.log("rendered");
 });
 
 app.get("/FBLA/reports", async (req, res) => {
+    // Generates a list of every student. This is only called when the page is accessed. This will not run when you use the filter. For that function, see the post request below
     let schoolStudents = [];
     let teacher = await Teacher.findById(signedInUser._id);
     let school = await School.findById(teacher.schoolId);
@@ -107,64 +108,39 @@ app.get("/FBLA/reports", async (req, res) => {
         schoolStudents.push(student);
     }
 
-    // let gradesStudents = [];
-
-    // for (let i = 0; i < grades.length; i++) {
-    //     let students = schoolStudents.filter(function (el) {
-    //         return el.grade === grades[i];
-    //     });
-    //     gradesStudents.push(students);
-    // }
-
-    // console.log(gradesStudents);
-
-    let displayedStudents = schoolStudents;
+    let displayedStudents = schoolStudents; // Display every student until a filter is applied
 
     res.render('reports', {
         displayedStudents,
         grades
     });
-    console.log("rendered");
 });
 
 app.post("/FBLA/reports", async (req, res) => {
+    // Same function as above, but it filters the students based on the grade selected
     let schoolStudents = [];
     let teacher = await Teacher.findById(signedInUser._id);
     let school = await School.findById(teacher.schoolId);
     let grades = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
     let selectedGrade = req.body.grade;
-    console.log(selectedGrade);
 
     for (let i = 0; i < school.students.length; i++) {
         let student = await Student.findById(school.students[i]);
         schoolStudents.push(student);
     }
 
-    console.log(schoolStudents);
-
-    // let gradesStudents = [];
-
-    // for (let i = 0; i < grades.length; i++) {
-    //     let students = schoolStudents.filter(function (el) {
-    //         return el.grade === grades[i];
-    //     });
-    //     gradesStudents.push(students);
-    // }
-
     let displayedStudents = schoolStudents.filter(function (el) {
         return el.grade === parseInt(selectedGrade);
     });
-
-    console.log(displayedStudents);
 
     res.render('reports', {
         displayedStudents,
         grades
     });
-    console.log("rendered");
 });
 
 app.get("/FBLA/winner", async (req, res) => {
+    // Generates a list of students from the school and sorts them by grade level. Then, it selects the student with the highest points overall from the school as well as random winners from each grade level
     let schoolStudents = [];
     let teacher = await Teacher.findById(signedInUser._id);
     let school = await School.findById(teacher.schoolId);
@@ -175,7 +151,7 @@ app.get("/FBLA/winner", async (req, res) => {
 
     grades = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-    let gradesStudents = [];
+    let gradesStudents = []; // Students sorted by grade level
 
     // Sorts the students by grade level
     for (let i = 0; i < grades.length; i++) {
@@ -185,11 +161,9 @@ app.get("/FBLA/winner", async (req, res) => {
         gradesStudents.push(students);
     };
 
-    console.log(gradesStudents);
+    allWinners = []; // This will hold all of the winners
 
-    allWinners = [];
-
-    // Selects the student with the highest points overall from the school
+    // Basic sorting algorithm to select the student with the highest points overall from the school
     let highestPointsStudent = null;
     let highestPoints = -Infinity;
 
@@ -200,10 +174,12 @@ app.get("/FBLA/winner", async (req, res) => {
         }
     };
 
+    // The data must be converted to a JSON object in order to be manipulated. MongoDB does not allow you to add new properties to a document object without modifying the database.
     highestPointsStudent = JSON.parse(JSON.stringify(highestPointsStudent));
-    console.log(highestPointsStudent);
-    // Add the reason for winning to the student object
+    // Add the reason for winning to the student object. This will be displayed on the winner page
     highestPointsStudent["reason"] = "Overall Winner";
+
+    // Get the prize. This could be converted to a loop, but this was easier to implement. If more prizes are added, this will need to be modified for conciseness
     if (highestPointsStudent.points >= 250) {
         highestPointsStudent["prize"] = "2 Free Prom Tickets OR a Free Yearbook";
     } else if (highestPointsStudent.points >= 200) {
@@ -218,7 +194,7 @@ app.get("/FBLA/winner", async (req, res) => {
 
     allWinners.push(highestPointsStudent);
 
-    // Using the gradesStudents array, select a student from each grade level.
+    // Using the gradesStudents array, select a random student from each grade level.
     // It will be added to the allWinners array
     let currentGrade = null;
     let randomWinner = null;
@@ -226,8 +202,8 @@ app.get("/FBLA/winner", async (req, res) => {
     for (let i = 0; i < gradesStudents.length; i++) {
         currentGrade = gradesStudents[i];
         randomWinner = currentGrade[Math.floor(Math.random() * currentGrade.length)];
-        console.log(randomWinner);
-        // If the random winner is undefined, then there are no students in that grade level, so skip it
+        
+        // If there are no students in the grade, randomWinner will be undefined. This will cause an error when the JSON object is parsed. This will skip the grade if there are no students in it
         if (randomWinner == undefined) {
             continue;
         }
@@ -244,38 +220,20 @@ app.get("/FBLA/winner", async (req, res) => {
         } else {
             randomWinner["prize"] = "Candy Bar";
         }
+
+        // Add the reason for winning to the student object. This will be displayed on the winner page
         randomWinner["reason"] = "Random Winner (Grade " + grades[i] + ")";
         allWinners.push(randomWinner);
 
     }
 
-
-
-
-    // let gradeNineWinner = gradeNine[Math.floor(Math.random() * gradeNine.length)];
-    // let gradeTenWinner = gradeTen[Math.floor(Math.random() * gradeTen.length)];
-    // let gradeElevenWinner = gradeEleven[Math.floor(Math.random() * gradeEleven.length)];
-    // let gradeTwelveWinner = gradeTwelve[Math.floor(Math.random() * gradeTwelve.length)];
-    // let allWinners = [];
-    // if (gradeNineWinner != undefined) {
-    //     allWinners.push(gradeNineWinner);
-    // }
-    // if (gradeTenWinner != undefined) {
-    //     allWinners.push(gradeTenWinner);
-    // }
-    // if (gradeElevenWinner != undefined) {
-    //     allWinners.push(gradeElevenWinner);
-    // }
-    // if (gradeTwelveWinner != undefined) {
-    //     allWinners.push(gradeTwelveWinner);
-    // }
     res.render('winner', {
         allWinners
     });
-    console.log("rendered");
 });
 
 app.post("/FBLA/AddEvent", (req, res) => {
+    // Adds the event to the database and adds the event to the school's events array
     Teacher.findById(signedInUser._id, (err, data) => {
         if (err) throw err;
         School.findById(data.schoolId, (error, school) => {
@@ -283,100 +241,102 @@ app.post("/FBLA/AddEvent", (req, res) => {
                 School.findByIdAndUpdate(school._id, { $push: { events: event._id } }, (er, schoolEvnt) => {
                     if (er) throw er;
                 });
-                res.redirect('/FBLA/events');
+                res.redirect('/FBLA/events'); // Sends the user back to the events page so they can see the new event
             });
         });
     });
 });
 
 app.post("/FBLA/EditEvent", async (req, res) => {
+    // If they did not select the event, redirect them back to the events page
     if (req.body.selectedId == "none") {
         res.redirect('/FBLA/events');
         return;
     }
-    let editedEvent = await Event.findByIdAndUpdate(req.body.selectedId, req.body);
+    let editedEvent = await Event.findByIdAndUpdate(req.body.selectedId, req.body); // The variable is not used. This is just to update the event in the database
     res.redirect('/FBLA/events');
 });
 
 app.post("/FBLA/SignUp", async (req, res) => {
+    // This creates a new user and a new school if the school does not exist
     let data = req.body;
     let school = await School.findOne({ name: data.school });
-    if (school != null) {
+    if (school != null) { // If the school exists, set the schoolId to the school's id
         data.schoolId = school._id;
     }
-    else {
+    else { // Create a new school
         data.schoolId = (await School.create({ name: data.school }))._id;
     }
     Teacher.findOne({ username: data.username }, (error, user) => {
-        if (user) {
+        if (user) { // If the username is already taken, redirect them back to the home page
             res.redirect('/FBLA');
         }
-        else if (data.confirmPassword === data.password) {
-            console.log(data);
+        else if (data.confirmPassword === data.password) { // If the passwords match, create the user
             Teacher.create(data, (error, args) => {
                 if (error) throw error;
-                console.log("Hello");
                 res.redirect('/FBLA');
             });
         }
-        else {
+        else { // This should only run if the passwords do not match. We move this to javascript verification if time permits
             res.redirect('/FBLA');
         }
     });
 });
 
 app.post("/FBLA/Login", async (req, res) => {
+    // Simple login function
     const { username, password } = req.body;
+
+    // Find the teacher in the database
     Teacher.findOne({ username: username }, (error, user) => {
-        if (user) {
+        if (user) { // If they exist, compare the passwords
             bcrypt.compare(password, user.password, (error, same) => {
-                if (same) {
+                if (same) { // Log them in and redirect them to the dashboard
                     res.redirect('/FBLA/dashboard');
                     signedInUser = user;
                 }
-                else {
+                else { // If the passwords do not match, redirect them back to the home page
                     res.redirect('/FBLA');
                 }
             });
         }
-        else {
+        else { // If the user does not exist, redirect them back to the home page
             res.redirect('/FBLA');
         }
     });
 });
 
 app.post("/FBLA/AddStudent", (req, res) => {
+    // Currently unused. This may be needed if we add student manually rather than through points assignment
     Student.create(req.body, (err, data) => {
         res.redirect('/FBLA/dashboard');
     });
 });
 
 app.post("/FBLA/AssignPoints", async (req, res) => {
-    if (req.body.name == "none") {
-        res.redirect('/FBLA/dashboard');
+    // Runs when the user submits the form on the assign points page
+    if (req.body.name == "none") { // If they did not select an event, redirect them back to the assign points page
+        res.redirect('/FBLA/assign-points');
         return;
     }
-    console.log(req.body);
+
     let student;
     let teacher = await Teacher.findById(signedInUser._id);
     let school = await School.findById(teacher.schoolId);
     let event = await Event.findOne({ name: req.body.name });
     let existingStudent = await Student.findOne({ name: req.body.studentName, grade: req.body.grade });
-    if (existingStudent != null) {
+    if (existingStudent != null) { // If the student exists, update their points
         let existingStudentID = existingStudent._id;
-        console.log(existingStudent);
-        let numPoints = existingStudent.points + event.points;
-        console.log(numPoints);
+        let numPoints = existingStudent.points + event.points; // Update the points
         Student.findByIdAndUpdate(existingStudentID, { "points": numPoints }, (err, data) => {
             if (err) throw err;
-            console.log("Updated?");
-            res.redirect('/FBLA/dashboard');
+            res.redirect('/FBLA/assign-points');
         });
-    } else {
+    } else { // If the student does not exist, create them and add them to the school's students array
         student = await Student.create({ name: req.body.studentName, grade: req.body.grade, points: event.points });
         School.findByIdAndUpdate(school._id, { $push: { students: student._id } }, (err, schoolEvnt) => {
             if (err) throw err;
-            res.redirect('/FBLA/dashboard');
+            res.redirect('/FBLA/assign-points');
         });
     }
 });
